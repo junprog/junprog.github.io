f91f68a012bd57873616ff7b0aa7d95122330515
jun-note <jyunya.uchida@gmail.com>
2021-03-31 11:56:10 +0900

    Change run-local-script.sh to adress wsl

--- run-local-docker.sh
+++ run-local-docker.sh
@@ -3,18 +3,43 @@
 DOCKER_IMAGE=jekyll-app
 DOCKER_CONTAINER=jekyll-app
 
-if [ "$(docker image ls -q "${DOCKER_IMAGE}")" ]; then
-    echo "Docker image : ${DOCKER_IMAGE} is already existed."
 
-    docker run -it --rm -v ${PWD}:/usr/src/app \
-        --name ${DOCKER_CONTAINER} -p 4000:4000 \
-        ${DOCKER_IMAGE} bundle exec jekyll serve --future --host 0.0.0.0
+if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    echo "On WSL"
+
+    if [ "$(docker image ls -q "${DOCKER_IMAGE}")" ]; then
+        echo "Docker image : ${DOCKER_IMAGE} is already existed."
+
+        docker run -it --rm -v ${PWD}:/usr/src/app \
+            --name ${DOCKER_CONTAINER} -p 4000:4000 \
+            ${DOCKER_IMAGE} bundle exec jekyll serve --future --force_polling --host 0.0.0.0
+    else
+        echo "Create docker image : ${DOCKER_IMAGE}"
+
+        docker build -t ${DOCKER_IMAGE} .
+
+        docker run -it --rm -v ${PWD}:/usr/src/app \
+            --name ${DOCKER_CONTAINER} -p 4000:4000 \
+            ${DOCKER_IMAGE} bundle exec jekyll serve --future --force_polling --host 0.0.0.0
+    fi
+
 else
-    echo "Create docker image : ${DOCKER_IMAGE}"
+    echo "On not WSL"
 
-    docker build -t ${DOCKER_IMAGE} .
+    if [ "$(docker image ls -q "${DOCKER_IMAGE}")" ]; then
+        echo "Docker image : ${DOCKER_IMAGE} is already existed."
 
-    docker run -it --rm -v ${PWD}:/usr/src/app \
-        --name ${DOCKER_CONTAINER} -p 4000:4000 \
-        ${DOCKER_IMAGE} bundle exec jekyll serve --future --host 0.0.0.0
+        docker run -it --rm -v ${PWD}:/usr/src/app \
+            --name ${DOCKER_CONTAINER} -p 4000:4000 \
+            ${DOCKER_IMAGE} bundle exec jekyll serve --future --host 0.0.0.0
+    else
+        echo "Create docker image : ${DOCKER_IMAGE}"
+
+        docker build -t ${DOCKER_IMAGE} .
+
+        docker run -it --rm -v ${PWD}:/usr/src/app \
+            --name ${DOCKER_CONTAINER} -p 4000:4000 \
+            ${DOCKER_IMAGE} bundle exec jekyll serve --future --host 0.0.0.0
+    fi
+
 fi
