e884c75748a45d2b04722e4978ddc97aebb4d793
jyunya <jyunya.uchida@gmail.com>
2021-04-06 17:45:03 +0900

    Add rfcalc

---
+++ scripts/calc_receptive_fields.js
@@ -0,0 +1,30 @@
+const layerModule = ["Conv2D", "Pool2D"];
+
+var layers = document.getElementById("layers")
+
+function addLayer() {
+    // a 要素の作成と属性の指定
+    var newAnchor = document.createElement("div");
+
+    var newLayer = document.createTextNode( document.getElementById("favtext").value );
+
+    var kernel_size = document.createTextNode( document.getElementById("kernel_size").value );
+    var stride = document.createTextNode( document.getElementById("stride").value );
+    var padding = document.createTextNode( document.getElementById("padding").value );
+
+    newAnchor.appendChild( newLayer );
+    newAnchor.setAttribute("kernel_size", kernel_size );
+    newAnchor.setAttribute("stride", stride );
+    newAnchor.setAttribute("padding", padding );
+   
+    // div 要素の作成
+    var newLayer = document.createElement("div");
+    newLayer.appendChild ( newAnchor );
+   
+    // リストに追加
+    layers.appendChild( newLi );   
+}
+
+function deleteLayer() {
+    
+}
