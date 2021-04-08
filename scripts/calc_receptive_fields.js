2aa882887b07b0f26ff481a9730237ee968df3e7
Junya Uchida <jyunya.uchida@gmail.com>
2021-04-08 09:50:26 +0900

    途中

--- scripts/calc_receptive_fields.js
+++ scripts/calc_receptive_fields.js
@@ -2,29 +2,17 @@ const layerModule = {
     "Conv2D": ["kernel_size", ""],
     "Pool2D": []};
 
-var layers = document.getElementById("layers")
+let layers = [];
 
 function addLayer() {
-    // a 要素の作成と属性の指定
-    var newAnchor = document.createElement("div");
+    // htmlから情報を取得
+    let module_el = document.getElementById("module");
 
-    var newLayer = document.createTextNode( document.getElementById("module").value );
+    let kernel_size_el = document.getElementById("kernel_size");
+    let stride_el = document.getElementById("stride");
+    let padding_el = document.getElementById("padding");
 
-    var kernel_size = document.createTextNode( document.getElementById("kernel_size").value );
-    var stride = document.createTextNode( document.getElementById("stride").value );
-    var padding = document.createTextNode( document.getElementById("padding").value );
-
-    newAnchor.appendChild( newLayer );
-    newAnchor.setAttribute("kernel_size", kernel_size );
-    newAnchor.setAttribute("stride", stride );
-    newAnchor.setAttribute("padding", padding );
-   
-    // div 要素の作成
-    //var newLayer = document.createElement("div");
-    //newLayer.appendChild( newAnchor );
-   
-    // リストに追加
-    //layers.appendChild( newAnchor );   
+ 
 }
 
 function deleteLayer() {
---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+var f = function(){
+    let module_el = document.getElementById("module");
+    console.log(module_el);
+};
+
+f.call();
+f.apply();
