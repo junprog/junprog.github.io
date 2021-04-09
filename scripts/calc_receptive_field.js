4f7b7ea0d69d096f5953975b6c8b19a9b63dfddc
jyunya <jyunya.uchida@gmail.com>
2021-04-09 13:35:16 +0900

    一応receptive field 計算機 完成

--- scripts/calc_receptive_field.js
+++ scripts/calc_receptive_field.js
@@ -3,10 +3,6 @@ class Layer {
         this.index = index;
         this.module = module
 
-        //this.input_size = parseInt(input_size, 10);
-        //this.output_size = calcOutputSize(input_size, kernel_size, stride, padding);
-        //global_input_size = this.output_size
-
         this.kernel_size = parseInt(kernel_size, 10);
         this.stride = parseInt(stride, 10);
         this.padding = parseInt(padding, 10);
@@ -16,6 +12,15 @@ class Layer {
         this.input_size = input_size
     }
 
+    setJump(pre_jump) {
+        this.pre_jump = pre_jump
+        this.jump = this.pre_jump * this.stride
+    }
+
+    setPreReceptiveField(pre_rf) {
+        this.pre_rf = pre_rf
+    }
+
     get output_size() {
         return this.calcOutputSize();
     }
@@ -23,11 +28,18 @@ class Layer {
     calcOutputSize() {
         return (Math.ceil((this.input_size - this.kernel_size + 2 * this.padding) / this.stride) + 1);
     }
+
+    get receptive_field() {
+        return this.calcReceptiveField();
+    }
+
+    calcReceptiveField() {
+        return (this.pre_rf + (this.kernel_size - 1) * this.pre_jump);
+    }
 }
 
 let layers = [];
 let global_index = 0;
-let global_input_size = 224;
 
 function addLayer() {
     if (layers.length > 50) {
@@ -85,7 +97,7 @@ function addLayer() {
 
             // 要素追加
             const layers_div_el = document.getElementById("layers");
-            if (module_el.value == "Conv2D"){
+            if (module_el.value == "Conv2d"){
                 const layer_block = document.createElement("div");
                 layer_block.classList.add("conv");
                 layer_block.id = global_index;
@@ -106,7 +118,7 @@ function addLayer() {
 
                 layers_div_el.appendChild(layer_block);
 
-            } else if (module_el.value == "Pool2D") {
+            } else if (module_el.value == "Pool2d") {
                 const layer_block = document.createElement("div"); 
                 layer_block.classList.add("pool");
                 layer_block.id = global_index;
@@ -139,15 +151,23 @@ function deleteLayer(index) {
     layers.splice(index, 1);
 }
 
-function deleteAllLayer() {
+function deleteAllLayers() {
     const layers_el = document.getElementById("layers");
     while(layers_el.firstChild){
         layers_el.removeChild(layers_el.firstChild);
     }
     layers = [];
+    global_index = 0;
 }
 
-function calcOutputSize() {
+function deleteAllResults() {
+    const layers_el = document.getElementById("results");
+    while(layers_el.firstChild){
+        layers_el.removeChild(layers_el.firstChild);
+    }
+}
+
+function calcInfo() {
     let input_size_el = document.getElementById("input_size");
     let input_size = parseInt(input_size_el.value, 10);
 
@@ -157,26 +177,37 @@ function calcOutputSize() {
     }
 
     for (let i = 0; i < layers.length; i++) {
+        // 入力サイズ指定
         layers[i].setInputSize(input_size);
 
-        if (layers[i].module == "Conv2D"){
+        // ジャンプ計算 & 前レイヤのRF指定
+        if (i == 0){
+            layers[i].setJump(1);
+            layers[i].setPreReceptiveField(1);
+        } else {
+            layers[i].setJump(layers[i-1].jump);
+            layers[i].setPreReceptiveField(layers[i-1].receptive_field);
+        }
+        
+
+        if (layers[i].module == "Conv2d"){
             const result_block = document.createElement("div");
             result_block.classList.add("conv");
 
             // ブロック内の文字
-            const result_info = layers[i].index + ": " + layers[i].module + "input_size=" + layers[i].input_size + ", output_size=" + layers[i].output_size;
+            const result_info = layers[i].index + ": " + layers[i].module + " input_size=" + layers[i].input_size + ", output_size=" + layers[i].output_size + ", receptive_filed=" + layers[i].receptive_field;
             const text1 = document.createTextNode(result_info);
 
             result_block.appendChild(text1);
 
             results_div_el.appendChild(result_block);
 
-        } else if (layers[i].module == "Pool2D") {
+        } else if (layers[i].module == "Pool2d") {
             const result_block = document.createElement("div");
             result_block.classList.add("pool");
 
             // ブロック内の文字
-            const result_info = layers[i].index + ": " + layers[i].module + "input_size=" + layers[i].input_size + ", output_size=" + layers[i].output_size;
+            const result_info = layers[i].index + ": " + layers[i].module + " input_size=" + layers[i].input_size + ", output_size=" + layers[i].output_size + ", receptive_filed=" + layers[i].receptive_field;
             const text1 = document.createTextNode(result_info);
 
             result_block.appendChild(text1);
@@ -185,4 +216,22 @@ function calcOutputSize() {
         }
         input_size = layers[i].output_size
     }   
+}
+
+function setDefault() {
+    let module_el = document.getElementById("module");
+
+    let kernel_size_el = document.getElementById("kernel_size");
+    let stride_el = document.getElementById("stride");
+    let padding_el = document.getElementById("padding");
+
+    if (module_el.value == "Conv2d") {
+        kernel_size_el.value = 3;
+        stride_el.value = 1;
+        padding_el.value = 1;
+    } else if (module_el.value == "Pool2d") {
+        kernel_size_el.value = 2;
+        stride_el.value = 2;
+        padding_el.value = 0;
+    }
 }
